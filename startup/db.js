8fc9ba6e0fbf900283e8c51784d13c17908de3d4
kingsley-4123 <ekenebtcboy@gmail.com>
2025-09-03 17:51:18 +1100

    uninstalled winston

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-import winston from "winston";
 
 function dbConnection() {
     
     mongoose.connect(process.env.MONGODB_URL)
-        .then(() => winston.info('Connection to DB was successful...'))   
+        .then(() => console.log('Connection to DB was successful...'))   
     
     //why we are not catching rejected promise is because we defined a global
     //unhandled promise rejection.
