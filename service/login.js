86780bdef6e10bd5ce8e2d735b36fdaef151bf30
kingsley-4123 <ekenebtcboy@gmail.com>
2025-11-05 18:20:49 +1100

    Made some modification in login response

--- service/login.js
+++ service/login.js
@@ -1,36 +1,37 @@
-import bcrypt from 'bcrypt';
-import { User } from '../models/user.js';
-import Joi from 'joi';
-import lodash from 'lodash';
+import bcrypt from "bcrypt";
+import { User } from "../models/user.js";
+import Joi from "joi";
+import lodash from "lodash";
 
 async function verifyUser(req, res) {
-    const { error } = validateLogin(req.body ); 
-    if (error) return res.status(400).json({message: error.details[0].message});
+  const { error } = validateLogin(req.body);
+  if (error) return res.status(400).json({ message: error.details[0].message });
 
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(401).json({message: 'Invalid email or password.'});
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return res.status(404).json({ message: "Invalid email or password." });
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(401).json({message: 'Invalid password.'});
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(400).json({ message: "Invalid email or password." });
 
-    const token = user.generateToken();
-    
-    res.json({
-        ok: true,
-        userId: user._id,
-        user: lodash.pick(user, ['firstname', 'middlename', 'surname', 'email']),
-        token
-    });
+  const token = user.generateToken();
 
+  res.json({
+    ok: true,
+    userId: user._id,
+    user: lodash.pick(user, ["firstname", "middlename", "surname", "email"]),
+    token
+  });
 }
 
-function validateLogin(credentials){
-    const schema = Joi.object({
-        email: Joi.string().email().min(5).max(255).required(),
-        password: Joi.string().min(5).required()
-    });
+function validateLogin(credentials) {
+  const schema = Joi.object({
+    email: Joi.string().email().min(5).max(255).required(),
+    password: Joi.string().min(5).required(),
+  });
 
-    return schema.validate(credentials);
+  return schema.validate(credentials);
 }
 
 export default verifyUser;
